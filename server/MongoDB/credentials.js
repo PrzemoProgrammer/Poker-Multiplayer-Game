eca2fb12de8961d84a2008a934baaa7f6bbd2334
PrzemoProgrammer <110130880+PrzemoProgrammer@users.noreply.github.com>
2023-10-14 20:14:25 +0200

    update

--- server/MongoDB/credentials.js
+++ server/MongoDB/credentials.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
 const HOST = process.env.HOST;
-const DB_URL =
-  "mongodb+srv://ptaku19:a4bmSABLa4bmSABLa4bmSABL@slotmachine.maj0ei5.mongodb.net/?retryWrites=true&w=majority";
+const DB_URL = process.env.DB_URL;
 
 module.exports = { DB_URL, USERNAME, PASSWORD, HOST };
