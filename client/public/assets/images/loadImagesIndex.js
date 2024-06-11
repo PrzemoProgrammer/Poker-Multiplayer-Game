3f9936e67ff71238efdccc63a7d8b1ba066509a2
PrzemoProgrammer <110130880+PrzemoProgrammer@users.noreply.github.com>
2024-06-11 20:19:58 +0200

    update

--- client/public/assets/images/loadImagesIndex.js
+++
@@ -1,6 +0,0 @@
-import loadGameImagesData from "./game/loadGameImagesData.json";
-import loadInterfaceImagesData from "./interface/loadInterfaceImagesData.json";
-import loadLoginScreenImagesData from "./loginScreen/loadLoginScreenImagesData.json";
-import loadRegisterScreenImagesData from "./registerScreen/loadRegisterScreenImagesData.json";
-
-export default [loadGameImagesData, loadInterfaceImagesData, loadLoginScreenImagesData, loadRegisterScreenImagesData]
